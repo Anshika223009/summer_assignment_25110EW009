a79cabbe41e6283a3c596c261e247b0c14dc68c4
Anshika kesarwani <anshikakesharvani478@gmail.com>
2026-06-01 16:29:18 +0530

    day-01

---
+++ sumofnaturalnumber.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int number;
+    int sum=0;
+    printf("Enter a postive integer:");
+    scanf("%d",&number);
+    for(int i=1;i<=number;i++){
+        sum+=i;
+
+    }
+    printf("Sum of N natural number is %d",sum);
+    return 0;
+}
