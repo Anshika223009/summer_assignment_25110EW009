781435b368d7284b1dd619950bd156da00eb2f78
Anshika kesarwani <anshikakesharvani478@gmail.com>
2026-06-21 17:44:33 +0530

    DAY21-uploaded

---
+++ DAY21/convert-lowercase_to_uppercase-letters.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    char str[100];
+
+    printf("enter a string:");
+    gets(str);
+
+    for( i = 0; str[i] != '\0'; i++)
+    {
+        if(str[i] >= 'a' && str[i] <= 'z')
+        {
+            str[i] = str[i] - 32;
+        }
+    }
+
+     printf("Uppercase string = %s", str);
+
+     return 0;
+}
---
+++ DAY21/count-vowels-consonants.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() 
+{
+    int i, vowels = 0, consonants = 0;
+    char str[100];
+
+
+    printf("Enter a string: ");
+    gets(str);
+
+    for (i = 0; str[i] != '\0'; i++) 
+    {
+        if ((str[i] >= 'A' && str[i] <= 'Z') ||
+            (str[i] >= 'a' && str[i] <= 'z')) 
+        {
+
+            if (str[i] == 'A' || str[i] == 'E' || str[i] == 'I' ||
+                str[i] == 'O' || str[i] == 'U' || str[i] == 'a' ||
+                str[i] == 'e' || str[i] == 'i' || str[i] == 'o' ||
+                str[i] == 'u')
+                vowels++;
+            else
+                consonants++;
+        }
+    }
+
+    printf("Vowels = %d\n", vowels);
+    printf("Consonants = %d", consonants);
+
+    return 0;
+}
